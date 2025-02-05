c566fe924abc1dc48b8e9f3757fb73fa7dcd8a8f
Pisky02 <114362821+Pisky02@users.noreply.github.com>
2025-02-05 11:48:11 +0100

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,37 +1,16 @@
 function Header() {
-  /*const scrollToSection = () => {
-    /**
-     * Najdu vysku headeru
-     * najdu contact section
-     * nejdu margin nad contact section
-     * zjistim kde je contact info na strance
-     * vypocitam kam musim az musim scrollnout
-     * *
-    const header = document.getElementById("header");
-    if (header) {
-      const headerHeight = header.offsetHeight;
-
-      const contactSection = document.getElementById("gallery"); ///CHANGE
-      if (contactSection) {
-        const contactSectionStyle = window.getComputedStyle(contactSection);
-        const marginTop = parseFloat(contactSectionStyle.marginTop);
-
-        const contactSectionPosition =
-          contactSection.getBoundingClientRect().top + window.scrollY;
-
-        window.scrollTo({
-          top: contactSectionPosition - headerHeight - marginTop,
-          behavior: "smooth",
-        });
-      }
-    }
-  };*/
-  const scrollToSection = (id) => {
+  const scrollToSection = (id: string) => {
     document.getElementById(id)?.scrollIntoView({ behavior: "smooth" });
   };
 
   return (
     <header id="header" className="header">
+      <button
+        className="scroll-button"
+        onClick={() => scrollToSection("basic-info")}
+      >
+        zakladni info
+      </button>
       <button
         className="scroll-button"
         onClick={() => scrollToSection("gallery")}
