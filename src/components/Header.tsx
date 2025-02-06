b923752809fec97484a6b2f93fcbb6a2126d898e
Pisky02 <114362821+Pisky02@users.noreply.github.com>
2025-02-06 18:36:53 +0100

    image change

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,7 +11,7 @@ function Header() {
         className="home"
         style={{
           height: "100vh",
-          width: "100vw",
+          width: "auto",
           backgroundImage: `url(${bgImage})`,
           backgroundSize: "cover",
           backgroundPosition: "center",
