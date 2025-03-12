f28a2b165ebd6dfcc2a75f3e83fd5997628cf7e2
Pisky02 <114362821+Pisky02@users.noreply.github.com>
2025-03-12 19:14:09 +0100

    im lost

--- src/components/BasicInfo.tsx
+++ src/components/BasicInfo.tsx
@@ -2,6 +2,7 @@ function BasicInfo() {
   return (
     <div id="basic-info">
       <h1>Zakladni info o svatbe.</h1>
+      <div className="text-blue-2000">Hello, Tailwind!</div>
       <div className="date">7.8.2025</div>
       <div className="start"> Začátek obřadu ve 13:00 hodin</div>
       <div className="place"> Celestia house, Velké opatovice</div>
